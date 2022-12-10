2fd0dd30700607cad14cf28289b6475ac7aad176
Delioos <htz_j@protonmail.com>
2022-12-10 23:06:55 +0100

    script + bat file

---
+++ quatreTier.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Users\Utilisateur\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.9"
+python "D:\Users\Utilisateur\Documents\code\python\cryptographie\resolutionPicker\main.py"
